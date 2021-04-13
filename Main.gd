469ed5b39fa5801e0890c059cd87209bfa40db83
Yurii Mazurevych <jmazurevych@gmail.com>
2021-04-13 12:44:24 +0300

    Small update

--- Main.gd
+++ Main.gd
@@ -15,36 +15,36 @@ func _ready():
 	db.query("DROP TABLE lobby")
 	db.query("DROP TABLE hardware")
 	db.query("""CREATE TABLE security  (
-    badge_number CHAR(5) NOT NULL PRIMARY KEY,
-    sign_out_date DATE,
-    sign_out_time TIME,
-    sign_in_time TIME
-    )""")
+	badge_number CHAR(5) NOT NULL PRIMARY KEY,
+	sign_out_date DATE,
+	sign_out_time TIME,
+	sign_in_time TIME
+	)""")
 	db.query("""CREATE TABLE employee (
-    employee_id CHAR(6) NOT NULL PRIMARY KEY,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    location CHAR(6),
-    manager_id CHAR(6),
-    extension CHAR(4),
-    gender CHAR(1),
-    hair_colour CHAR(6),
-    badge_number CHAR(5) REFERENCES security (badge_number),
-    restricted_access CHAR(1)
-    )""")
+	employee_id CHAR(6) NOT NULL PRIMARY KEY,
+	first_name VARCHAR(20),
+	last_name VARCHAR(20),
+	location CHAR(6),
+	manager_id CHAR(6),
+	extension CHAR(4),
+	gender CHAR(1),
+	hair_colour CHAR(6),
+	badge_number CHAR(5) REFERENCES security (badge_number),
+	restricted_access CHAR(1)
+	)""")
 	db.query("""CREATE TABLE lobby (
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    badge_number CHAR(5) NOT NULL REFERENCES security (badge_number),
-    PRIMARY KEY (first_name, last_name)
-    )""")
+	first_name VARCHAR(20),
+	last_name VARCHAR(20),
+	badge_number CHAR(5) NOT NULL REFERENCES security (badge_number),
+	PRIMARY KEY (first_name, last_name)
+	)""")
 	db.query("""CREATE TABLE hardware (
-    employee_id CHAR(6) PRIMARY KEY,
-    location CHAR(6),
-    hardware_tn CHAR(8),
-    hardware_sn CHAR(8) NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
-    )""")
+	employee_id CHAR(6) PRIMARY KEY,
+	location CHAR(6),
+	hardware_tn CHAR(8),
+	hardware_sn CHAR(8) NOT NULL,
+	FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
+	)""")
 	print("There are ", db.query_result)
 	load_data(db)
 	db.close_db()
